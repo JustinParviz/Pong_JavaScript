a3ca940538bc94c2cadf5454de5ebc17f5c19b69
JustinParviz <jarshravan@gmail.com>
2025-01-16 17:56:22 -0500

    declared all const and let variables

---
+++ index.js
@@ -0,0 +1,39 @@
+const gameBoard = document.querySelector("#gameBoard");
+const ctx = gameBoard.getContext("2d");
+const scoreText = document.querySelector("#scoreText");
+const resetBtn = document.querySelector("#resetBtn");
+const gameWidth = gameBoard.width;
+const gameHeight = gameBoard.height;
+const boardBackground = "forestgreen";
+const paddle1Color = "lightblue";
+const paddle2Color = "red";
+const paddleBorder = "black";
+const ballColor = "yellow";
+const ballBorderColor = "black";
+const ballRadius = 12.5;
+const paddleSpeed = 50;
+let intervalID;
+let ballSpeed = 1;
+let ballX = gameWidth / 2;      // ballX and ballY are coordinates
+let ballY = gameHeight / 2;
+let ballXDirection = 0;         // ballXDirection is the direction in which the ball is headed on the X axis
+let ballYDirection = 0;         // ballYDirection is the direction in which the ball is headed on the Y axis
+let player1Score = 0;
+let player2Score = 0;
+let paddle1 = {
+    width: 25,
+    height: 100,                // the width and height for both paddles are in pixels
+    x: 0,
+    y: 0                        // these coordinates will put paddle 1 in the top left corner initially
+}
+let paddle2 = {
+    width: 25,
+    height: 100,
+    x: gameWidth - 25,
+    y: gameHeight - 100         // these coordinates will put paddle 2 on the other side of the canvas initially
+}
+
+
+
+
+
